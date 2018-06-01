daa7e352a09c46f4f82da1e6cf8f11538605ad60
doissegundos <39842850+doissegundos@users.noreply.github.com>
2018-06-01 19:07:12 -0300

    Create Operações.java

---
+++ Operações.java
@@ -0,0 +1,38 @@
+public class Operações {
+    
+    private String operações;
+    private boolean binario;
+    
+    public Operações(String operações,boolean binario) {
+        this.operações = operações;
+        this.binario= binario;
+    }
+
+    
+    public boolean getBinario() {
+        return binario;
+    }
+
+    public void setBinario(boolean binario) {
+        this.binario = binario;
+    }
+
+    public String getOperações() {
+        return operações;
+    }
+
+    public void setOperações(String operações) {
+        this.operações = operações;
+    }
+
+        
+    @Override
+    public String toString() {
+        return operações;
+    }
+    
+    
+    
+    
+    
+}
